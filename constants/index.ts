600ac23b80d702d74af0b197767dad316c18d4e1
arafat-hasan <info.arafat69@gmail.com>
2023-12-06 20:31:25 +0600

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,90 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "how_hilink_work", label: "How Hilink Work?" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "pricing ", label: "Pricing " },
+  { href: "/", key: "contact_us", label: "Contact Us" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Real maps can be offline",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
+  },
+  {
+    title: "Set an adventure schedule",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
+  },
+  {
+    title: "Technology using augment reality",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection",
+  },
+  {
+    title: "Many new locations every month",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      { text: "About Hilink", url: "/about-hilink" },
+      { text: "Press Releases", url: "/press-releases" },
+      { text: "Environment", url: "/environment" },
+      { text: "Jobs", url: "/jobs" },
+      { text: "Privacy Policy", url: "/privacy-policy" },
+      { text: "Contact Us", url: "/contact-us" },
+    ],
+  },
+  {
+    title: "Our Community",
+    links: [
+      { text: "Climbing xixixi", url: "/climbing-xixixi" },
+      { text: "Hiking hilink", url: "/hiking-hilink" },
+      { text: "Hilink kinthill", url: "/hilink-kinthill" },
+      { text: "New Community Link", url: "/new-community-link" },
+    ],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "hilink@akinthil.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    { icon: "/facebook.svg", url: "https://www.facebook.com/imarafathasan" },
+    { icon: "/instagram.svg", url: "https://www.instagram.com/imarafathasan" },
+    { icon: "/twitter.svg", url: "https://twitter.com/aisthrilling" },
+    { icon: "/youtube.svg", url: "https://www.youtube.com/@imarafathasan" },
+  ],
+};
